a29899993f36d624924f5bec2900230228e3a58c
yuiseki <yuiseki@gmail.com>
2024-03-31 15:23:07 +0900

    30分で学習できる条件

--- src/dataset/text.py
+++ src/dataset/text.py
@@ -1,13 +1,13 @@
 import datasets
 
 wikipedia_en_dataset = datasets.load_dataset("wikimedia/wikipedia", "20231101.en", split="train")
-for data in wikipedia_en_dataset.filter(lambda data, indice: indice % 10 == 0, with_indices=True):
+for data in wikipedia_en_dataset.filter(lambda data, indice: indice % 50 == 0, with_indices=True):
     if data["text"] == "" or data["text"] == "\n":
         continue
     print(data["text"])
 
 wikipedia_ja_dataset = datasets.load_dataset("wikimedia/wikipedia", "20231101.ja", split="train")
-for data in wikipedia_ja_dataset.filter(lambda data, indice: indice % 2 == 0, with_indices=True):
+for data in wikipedia_ja_dataset.filter(lambda data, indice: indice % 10 == 0, with_indices=True):
     if data["text"] == "" or data["text"] == "\n":
         continue
     print(data["text"])
