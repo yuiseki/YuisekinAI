455ff95b3022519b5766f32db9f09bc56f358ab4
yuiseki <yuiseki@gmail.com>
2024-03-31 13:49:15 +0900

    rename *.sh

---
+++ 003_tokenizer_train.sh
@@ -0,0 +1,1 @@
+python src/tokenizer/train.py
--- pretrain.sh
+++ pretrain.sh
@@ -1,1 +1,1 @@
-deepspeed --no_local_rank src/run_clm.py --output_dir checkpoints/YuisekinAI-mistral-300M-FA2 hf_config.json --deepspeed --deepspeed_config ds_config_zero.json
+deepspeed --no_local_rank src/run_clm.py hf_config.json --deepspeed --deepspeed_config ds_config_zero.json
