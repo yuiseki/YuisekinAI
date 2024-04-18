d6ff96270b2f42bff6f5d96716de35724516daa4
yuiseki <yuiseki@gmail.com>
2024-04-19 08:20:48 +0900

    update src/dataset/text.py

--- src/dataset/text.py
+++ src/dataset/text.py
@@ -1,13 +1,35 @@
 import datasets
 
 wikipedia_en_dataset = datasets.load_dataset("wikimedia/wikipedia", "20231101.en", split="train")
-for data in wikipedia_en_dataset.filter(lambda data, indice: indice % 2 == 0, with_indices=True):
+for data in wikipedia_en_dataset:
+    # 空の行は除去
+    # 改行のみの行は除去
     if data["text"] == "" or data["text"] == "\n":
         continue
     print(data["text"])
 
+openmath_en_dataset = datasets.load_dataset("nvidia/OpenMathInstruct-1", split="train")
+for data in openmath_en_dataset:
+    if data["is_correct"] is False:
+        continue
+    print(data["question"])
+    print(data["generated_solution"])
+
+wizardlm_en_dataset = datasets.load_dataset("WizardLM/WizardLM_evol_instruct_70k", split="train")
+for data in wizardlm_en_dataset:
+    print(data["instruction"])
+    print(data["output"])
+
+dolphin_en_dataset = datasets.load_dataset("cognitivecomputations/dolphin", "flan1m-alpaca-uncensored", split="train")
+for data in dolphin_en_dataset:
+    print(data["instruction"])
+    print(data["input"])
+    print(data["output"])
+
+"""
 wikipedia_ja_dataset = datasets.load_dataset("wikimedia/wikipedia", "20231101.ja", split="train")
 for data in wikipedia_ja_dataset:
     if data["text"] == "" or data["text"] == "\n":
         continue
     print(data["text"])
+"""
