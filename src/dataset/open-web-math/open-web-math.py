d76b623d6433750542a918ebe9491b68fab5a02a
yuiseki <yuiseki@gmail.com>
2025-03-16 12:43:45 +0900

    add src/dataset/open-web-math/open-web-math.py

---
+++ src/dataset/open-web-math/open-web-math.py
@@ -0,0 +1,16 @@
+import datasets
+
+# https://huggingface.co/datasets/open-web-math/open-web-math
+# License: ODC-By 1.0
+# Size: 27.4 GB
+# Token: 14.7 B
+
+dataset = datasets.load_dataset("open-web-math/open-web-math", split="train", trust_remote_code=True)
+for data in dataset:
+    # remove empty text
+    if data["text"] == "":
+      continue
+    # remove text with only a newline character
+    if data["text"] == "\n":
+        continue
+    print(data["text"])
