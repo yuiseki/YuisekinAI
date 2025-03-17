cb91ab0517e557825c931f03a663a00cc14cb8c2
yuiseki <yuiseki@gmail.com>
2025-03-17 16:57:28 +0900

    datasetのtokenizeに30分以上掛ける

--- 100_pretrain.sh
+++ 100_pretrain.sh
@@ -1,9 +1,11 @@
-export TORCH_DISTRIBUTED_TIMEOUT=7200000 # 2 時間
+export TORCH_DISTRIBUTED_TIMEOUT=7200
 export TORCH_DISTRIBUTED_DEBUG=DETAIL
-export DEEPSPEED_TIMEOUT=7200000 # 2 時間
+export DEEPSPEED_TIMEOUT=7200
 export NCCL_DEBUG=INFO
 export NCCL_SOCKET_NTHREADS=1
 export NCCL_NSOCKS_PERTHREAD=4
 export NCCL_SOCKET_IFNAME=lo
+
+env | grep TIMEOUT
 
 deepspeed --no_local_rank src/run_clm.py hf_config.json --deepspeed --deepspeed_config ds_config_zero.json
