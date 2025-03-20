6284b0eec09ec86d350aa9ff6b8c130cfbfda39d
yuiseki <yuiseki@gmail.com>
2025-03-20 12:01:36 +0900

    Update 100_pretrain.sh

--- 100_pretrain.sh
+++ 100_pretrain.sh
@@ -5,7 +5,8 @@ export NCCL_DEBUG=INFO
 export NCCL_SOCKET_NTHREADS=1
 export NCCL_NSOCKS_PERTHREAD=4
 export NCCL_SOCKET_IFNAME=lo
-
-env | grep TIMEOUT
+export NCCL_IB_DISABLE=1
+export NCCL_P2P_DISABLE=1
+export CUDA_DEVICE_MAX_CONNECTIONS=1
 
 deepspeed --no_local_rank src/run_clm.py hf_config.json --deepspeed --deepspeed_config ds_config_zero.json
